7a9aa46535ca270321bcb8be5aeb371a6b8593d3
rochamanuela <rochadesouzamanuela@gmail.com>
2024-07-16 15:34:13 -0300

    feat: create pages

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.css'
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Hii, você está na HOME</h1>
+        </div>
+    )
+}
+
---
+++ src/pages/Projects/FutManager/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './styles.css'
+
+export default function FutManager() {
+    return (
+        <div>
+            <h1>Hii, você está na FUTMANAGER</h1>
+        </div>
+    )
+}
+
