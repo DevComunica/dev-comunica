29707e09c71ed84705c8ca82f468552611002026
rochamanuela <rochadesouzamanuela@gmail.com>
2024-07-16 17:13:16 -0300

    feat: navbar finished

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { NavLink, useLocation } from 'react-router-dom'
+import Logotype from '../../assets/images/logotype.png'
 import './styles.css'
 
 export default function Navbar() {
+    const location = useLocation()
+
     return (
-        <div>
-            <h1>Hii, eu sou a NAVBAR</h1>
+        <div className='navbar-container'>
+            <img src={Logotype} className='logotype-dev-comunica' />
+            <div className='links'>
+                <NavLink exact to={'/'} className={location.pathname === '/' ? 'active-link' : ''}>Conheça PSI</NavLink>
+                <NavLink to={'/futmanager'} className={location.pathname === '/futmanager' ? 'active-link' : ''}>Projeto 1 - Futmanager</NavLink>
+                <NavLink to={'https://music.youtube.com/watch?v=lu0XVRP_Bfc&list=RDAMVMSeZouxOauPw'}>Redes Sociais</NavLink>
+            </div>
         </div>
     )
 }
--- src/pages/Projects/FutManager/index.jsx
+++ src/pages/Projects/FutManager/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Navbar } from '../../../components'
 import './styles.css'
 
 export default function FutManager() {
     return (
         <div>
+            <Navbar />
             <h1>Hii, você está na FUTMANAGER</h1>
         </div>
     )
