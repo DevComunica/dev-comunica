c20778d2478a0f50f0a3cf14551b78ff3cd91b2d
rochamanuela <rochadesouzamanuela@gmail.com>
2024-07-16 16:37:27 -0300

    feat: cleaning up default styles

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,13 @@
 import Navbar from './Navbar'
 import Footer from './Footer'
+import CardComment from './CardComment'
+import AutomaticSlider from './AutomaticSlider'
+import Slider from './Slider'
 
 export {
     Navbar,
-    Footer
+    Footer,
+    CardComment,
+    AutomaticSlider,
+    Slider
 }
