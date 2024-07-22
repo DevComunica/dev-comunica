b19ed7afcc912977581a1df7f056535397e01c09
rochamanuela <rochadesouzamanuela@gmail.com>
2024-07-22 08:00:28 -0300

    feat: continue footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,17 +1,59 @@
 import React from 'react'
+import { HiOutlineMail } from 'react-icons/hi'
+import { RiInstagramLine } from 'react-icons/ri'
+import { FaGithub } from 'react-icons/fa6'
+import LogotypeWhite from '../../assets/images/logotype-white.png'
 import './styles.css'
 
 export default function Footer() {
     return (
         <footer className='footer-container'>
-            <h1>Hii, eu sou o FOOTER</h1>
-            <div className="footer-final-line">
+            <div className='footer-main-content'>
+                <div className='item'>
+                    <img src={LogotypeWhite} />
+                </div>
+                <div className='item'>
+                    <a href="">Relatório da disciplina de PSI</a>
+                    <a href="">Documentação do projeto Futmanager</a>
+                </div>
+                <div className='item'>
+                    <div className='item-link'>
+                        <RiInstagramLine size={24} />
+                        <a href="">futpazescolinha</a>
+                    </div>
+                    <div className='item-link'>
+                        <FaGithub size={24} />
+                        <a href="">Futmanager</a>
+                    </div>
+                </div>
+                <div className='item'>
+                    <p>Contate-nos</p>
+                    <div className='btn-container'>
+                        <a href="">
+                            <button>Ir para o WhatsApp</button>
+                        </a>
+                        <a href="">
+                            <button>Enviar e-mail</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <hr />
+            <div className='footer-final-line'>
                 <p>© Todos os direitos reservados por Dev Comunica, IFSP Hortolândia</p>
-
-                <div className="links">
-                    <a href="">dev.comunicaifsp@gmail.com</a>
-                    <a href="">dev.comunica</a>
-                    <a href="">dev-comunica-web</a>
+                <div className='links'>
+                    <div className='item-link'>
+                        <HiOutlineMail size={24} />
+                        <a href="">dev.comunicaifsp@gmail.com</a>
+                    </div>
+                    <div className='item-link'>
+                        <RiInstagramLine size={24} />
+                        <a href="">dev.comunica</a>
+                    </div>
+                    <div className='item-link'>
+                        <FaGithub size={24} />
+                        <a href="">dev-comunica-web</a>
+                    </div>
                 </div>
             </div>
         </footer>
