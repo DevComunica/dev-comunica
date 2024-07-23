7046864c3806b3f14cb33df830c1ae5d09cf4feb
elisareys <elisasomavillar@gmail.com>
2024-07-23 19:15:27 -0300

    started the projects page

--- src/pages/Projects/FutManager/index.jsx
+++ src/pages/Projects/FutManager/index.jsx
@@ -7,8 +7,20 @@ export default function FutManager() {
         <div>
             <Navbar />
             <h1>Hii, você está na FUTMANAGER</h1>
+        <main className="Content">
+        <div className="Section" style={{ backgroundColor: 'lightblue' }}>Seção 1
+        <p>futmanager e fotinho note</p></div>
+
+        <div className="Section" style={{ backgroundColor: 'lightgreen' }}>Seção 2
+        <p>funcionalidades</p></div>
+
+        <div className="Section" style={{ backgroundColor: 'lightcoral' }}>Seção 3
+        <p>experiencia clientes</p></div>
+
+        <div className="Section" style={{ backgroundColor: 'lightgoldenrodyellow' }}>Seção 4
+        <p>futpaz e fotinho</p></div>
+        </main>
             <Footer />
         </div>
     )
 }
-
